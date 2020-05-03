6bf8c769cceb214bf5f45715afef1cba345f80e3
harikanjana <64727617+harikanjana@users.noreply.github.com>
2020-05-03 17:55:43 +0530

    Add files via upload

---
+++ capstone.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <u><b>this project is mainly about capstone</b></u>
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
